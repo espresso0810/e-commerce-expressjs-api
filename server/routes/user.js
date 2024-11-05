806f747403d52a0480e37e698f53c3931170fe60
hungp03 <ph.hung0903@gmail.com>
2024-11-05 17:03:40 +0700

    upload api

---
+++ server/routes/user.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const ctrls = require("../controllers/user");
+const { verifyAccessToken, isAdmin } = require("../middleware/verifyToken");
+
+router.post("/register", ctrls.register);
+router.put("/finalregister/:token", ctrls.finalRegister)
+router.post("/login", ctrls.login);
+router.get("/current", verifyAccessToken, ctrls.getCurrentUser);
+router.post("/refreshToken", ctrls.refreshAccessToken);
+router.post("/logout", ctrls.logout);
+router.post("/forgotpassword", ctrls.forgotPassword);
+router.put("/resetpassword", ctrls.resetPassword);
+router.get("/", [verifyAccessToken, isAdmin], ctrls.getUsers);
+router.delete("/delete", [verifyAccessToken, isAdmin], ctrls.deleteUser);
+router.put("/updateCurrent", [verifyAccessToken], ctrls.updateUser);
+router.put(
+  "/adminUpdate/:uid",
+  [verifyAccessToken, isAdmin],
+  ctrls.updateUserByAdmin
+);
+router.put("/address", [verifyAccessToken], ctrls.updateUserAddress);
+router.put("/cart", [verifyAccessToken], ctrls.updateCart);
+
+module.exports = router;
+
+//Post, put : req.body
+//Get, delete: req.query
